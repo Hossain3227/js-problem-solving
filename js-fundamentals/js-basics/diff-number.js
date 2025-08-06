799f665dda5db5c4d4b793999a6cfaaff920dc48
Hossain3227 <hasan32.hk27@gmail.com>
2025-08-07 02:30:12 +0600

    new diff problem added

---
+++ js-fundamentals/js-basics/diff-number.js
@@ -0,0 +1,16 @@
+// difference between a given number and 13, if the number is broader than 13 return double the absolute difference
+
+function difference(n){
+
+    if(n<=13){
+        return 13-n;
+    }
+    else{
+        return (n-13)*2;
+    }
+}
+
+console.log(difference(32));
+
+console.log(difference(11));
+
