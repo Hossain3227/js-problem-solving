f919b8608c96816e1d68ad01b3de70b2e8ecddb4
Hossain3227 <hasan32.hk27@gmail.com>
2025-08-07 03:09:18 +0600

    leap year added

---
+++ js-fundamentals/js-basics/leap-year.js
@@ -0,0 +1,11 @@
+// determine whether a given year is a leap year 
+
+function leapYear(year){
+return (year % 100 === 0)? (year%400 ===0) : (year%4===0);
+}
+
+console.log(leapYear(2016));
+console.log(leapYear(2000));
+console.log(leapYear(1700));
+console.log(leapYear(1800));
+console.log(leapYear(100));
