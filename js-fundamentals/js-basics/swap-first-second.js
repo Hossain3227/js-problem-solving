6d4dcbc4cb863fa0991ecaf3b3349af8016b6d45
Hossain3227 <hasan32.hk27@gmail.com>
2025-08-10 19:32:00 +0600

    swap problem added

---
+++ js-fundamentals/js-basics/swap-first-second.js
@@ -0,0 +1,16 @@
+// Write a JavaScript program to create a new string from a given string by changing the position of the first and last characters. The string length must be broader than or equal to 1
+
+
+function first_last(str1){
+    if(str1.length <= 1){
+        return str1;
+    }
+    mid_char = str1.substring(1,str1.length -1);
+
+    return (str1.charAt(str1.length - 1)+ mid_char + str1.charAt(0))
+
+}
+
+console.log(first_last('a'));
+console.log(first_last('ab'));
+console.log(first_last('abc'));
