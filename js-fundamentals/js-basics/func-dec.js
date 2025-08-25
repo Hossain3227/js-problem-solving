a510ffae9640ea19e6b9af90b403cf4955eee26a
Hossain3227 <hasan32.hk27@gmail.com>
2025-08-25 12:53:19 +0600

    new problem added

---
+++ js-fundamentals/js-basics/func-dec.js
@@ -0,0 +1,16 @@
+// function declaration
+
+function sayHello(username){
+    console.log(`hello ${username}`);
+    
+}
+
+sayHello("Zidan")
+
+//function Expression
+const greetings = function (user) {
+  
+    console.log(`hello ${user}`);
+};
+
+greetings("Zidan")
