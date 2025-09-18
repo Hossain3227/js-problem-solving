373b0d794ef540dc723720f2ab1d95cc8d478072
Hossain3227 <hasan32.hk27@gmail.com>
2025-09-18 19:15:49 +0600

    get more events

--- js-dom/dom_events/js/events.js
+++ js-dom/dom_events/js/events.js
@@ -1,1 +1,16 @@
 console.log('external file');
+
+function makeYellow(){
+    document.body.style.backgroundColor = 'yellow'
+}
+function makeRed(){
+    document.body.style.backgroundColor = 'red'
+}
+
+
+const btnMakeBlue = document.getElementById('btn-make-blue');
+// console.log(btnMakeBlue);
+btnMakeBlue.onclick = function makeBlue(){
+    document.body.style.backgroundColor = 'blue';
+}
+
