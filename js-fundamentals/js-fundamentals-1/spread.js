864cf039062d01a40bcdca893efba6efcc72b31f
Hossain3227 <hasan32.hk27@gmail.com>
2025-09-24 21:58:23 +0600

    spread operator

---
+++ js-fundamentals/js-fundamentals-1/spread.js
@@ -0,0 +1,12 @@
+// const friends = ['sam', 'jim', 'helen'];
+
+// console.log(...friends);
+
+// const newArray = ['hero','villain', ...friends];
+// console.log(newArray);
+
+const numbers = [1,2,3,4,5,6,7,8];
+
+console.log(Math.max(...numbers))
+
+
