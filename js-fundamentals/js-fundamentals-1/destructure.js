c28a37193f458261ef88189b3d66b8744fc69719
Hossain3227 <hasan32.hk27@gmail.com>
2025-09-24 22:24:37 +0600

    added obj destructure

---
+++ js-fundamentals/js-fundamentals-1/destructure.js
@@ -0,0 +1,24 @@
+const hena = {
+    name: 'hena',
+    age: 10,
+    friends: ['hero','bappa'],
+    status: "not found"
+}
+
+// const henaStatus = hena.status;
+// const henaName = hena.name;
+// const henaAge = hena.age;
+// const henaFriends = hena.friends;
+
+// const {name, age, status, friends} = hena
+
+// console.log(age);
+
+const person = ['sam',{name:'gias'},'kim','jim'];
+// console.log(person[0])
+
+const [nay, obj] = person;
+console.log(obj);
+
+
+
